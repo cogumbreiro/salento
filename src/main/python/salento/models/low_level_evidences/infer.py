ae6fd4f5539b844e5335279df99088a9e6567a27
Tiago Cogumbreiro <cogumbreiro@users.sf.net>
2018-06-04 16:45:35 -0400

    Fixed the dates of copyright.

--- src/main/python/salento/models/low_level_evidences/infer.py
+++ src/main/python/salento/models/low_level_evidences/infer.py
@@ -1,5 +1,5 @@
 # Copyright 2018 Georgia Tech
-# Copyright 2017 Rice University
+# Copyright 2017-2018 Rice University
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
